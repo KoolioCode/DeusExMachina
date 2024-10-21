f0b5f41fd945cc7e19605f8f7ab1f263a11f2fb2
ProgWithLog <120297920+KoolioCode@users.noreply.github.com>
2024-10-21 14:45:36 -0500

    Update DeusExMachina.bat

--- DeusExMachina.bat
+++ DeusExMachina.bat
@@ -75,10 +75,114 @@ REM Firewall
 netsh advfirewall set allprofiles state on
 
 
-REM Telnet
+REM Feature disable
 
-DISM /online /disable-feature /featurename:TelnetClient
-DISM /online /disable-feature /featurename:TelnetServer
+dism /online /disable-feature /featurename:IIS-WebServerRole >NUL
+dism /online /disable-feature /featurename:IIS-WebServer >NUL
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures >NUL
+dism /online /disable-feature /featurename:IIS-HttpErrors >NUL
+dism /online /disable-feature /featurename:IIS-HttpRedirect >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45 >NUL
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics >NUL
+dism /online /disable-feature /featurename:IIS-HttpLogging >NUL
+dism /online /disable-feature /featurename:IIS-LoggingLibraries >NUL
+dism /online /disable-feature /featurename:IIS-RequestMonitor >NUL
+dism /online /disable-feature /featurename:IIS-HttpTracing >NUL
+dism /online /disable-feature /featurename:IIS-Security >NUL
+dism /online /disable-feature /featurename:IIS-URLAuthorization >NUL
+dism /online /disable-feature /featurename:IIS-RequestFiltering >NUL
+dism /online /disable-feature /featurename:IIS-IPSecurity >NUL
+dism /online /disable-feature /featurename:IIS-Performance >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic >NUL
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools >NUL
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools >NUL
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility >NUL
+dism /online /disable-feature /featurename:IIS-Metabase >NUL
+dism /online /disable-feature /featurename:IIS-HostableWebCore >NUL
+dism /online /disable-feature /featurename:IIS-StaticContent >NUL
+dism /online /disable-feature /featurename:IIS-DefaultDocument >NUL
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing >NUL
+dism /online /disable-feature /featurename:IIS-WebDAV >NUL
+dism /online /disable-feature /featurename:IIS-WebSockets >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationInit >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET45 >NUL
+dism /online /disable-feature /featurename:IIS-ASP >NUL
+dism /online /disable-feature /featurename:IIS-CGI >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIFilter >NUL
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes >NUL
+dism /online /disable-feature /featurename:IIS-CustomLogging >NUL
+dism /online /disable-feature /featurename:IIS-BasicAuthentication >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic >NUL
+dism /online /disable-feature /featurename:IIS-ManagementConsole >NUL
+dism /online /disable-feature /featurename:IIS-ManagementService >NUL
+dism /online /disable-feature /featurename:IIS-WMICompatibility >NUL
+dism /online /disable-feature /featurename:IIS-LegacyScripts >NUL
+dism /online /disable-feature /featurename:IIS-LegacySnapIn >NUL
+dism /online /disable-feature /featurename:IIS-FTPServer >NUL
+dism /online /disable-feature /featurename:IIS-FTPSvc >NUL
+dism /online /disable-feature /featurename:IIS-FTPExtensibility >NUL
+dism /online /disable-feature /featurename:TFTP >NUL
+dism /online /disable-feature /featurename:TelnetClient >NUL
+dism /online /disable-feature /featurename:TelnetServer >NUL
+
+echo Re-Disable 
+
+dism /online /disable-feature /featurename:IIS-WebServerRole
+dism /online /disable-feature /featurename:IIS-WebServer
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures
+dism /online /disable-feature /featurename:IIS-HttpErrors
+dism /online /disable-feature /featurename:IIS-HttpRedirect
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics
+dism /online /disable-feature /featurename:IIS-HttpLogging
+dism /online /disable-feature /featurename:IIS-LoggingLibraries
+dism /online /disable-feature /featurename:IIS-RequestMonitor
+dism /online /disable-feature /featurename:IIS-HttpTracing
+dism /online /disable-feature /featurename:IIS-Security
+dism /online /disable-feature /featurename:IIS-URLAuthorization
+dism /online /disable-feature /featurename:IIS-RequestFiltering
+dism /online /disable-feature /featurename:IIS-IPSecurity
+dism /online /disable-feature /featurename:IIS-Performance
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility
+dism /online /disable-feature /featurename:IIS-Metabase
+dism /online /disable-feature /featurename:IIS-HostableWebCore
+dism /online /disable-feature /featurename:IIS-StaticContent
+dism /online /disable-feature /featurename:IIS-DefaultDocument
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing
+dism /online /disable-feature /featurename:IIS-WebDAV
+dism /online /disable-feature /featurename:IIS-WebSockets
+dism /online /disable-feature /featurename:IIS-ApplicationInit
+dism /online /disable-feature /featurename:IIS-ASPNET
+dism /online /disable-feature /featurename:IIS-ASPNET45
+dism /online /disable-feature /featurename:IIS-ASP
+dism /online /disable-feature /featurename:IIS-CGI
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions
+dism /online /disable-feature /featurename:IIS-ISAPIFilter
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes
+dism /online /disable-feature /featurename:IIS-CustomLogging
+dism /online /disable-feature /featurename:IIS-BasicAuthentication
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic
+dism /online /disable-feature /featurename:IIS-ManagementConsole
+dism /online /disable-feature /featurename:IIS-ManagementService
+dism /online /disable-feature /featurename:IIS-WMICompatibility
+dism /online /disable-feature /featurename:IIS-LegacyScripts
+dism /online /disable-feature /featurename:IIS-LegacySnapIn
+dism /online /disable-feature /featurename:IIS-FTPServer
+dism /online /disable-feature /featurename:IIS-FTPSvc
+dism /online /disable-feature /featurename:IIS-FTPExtensibility
+dism /online /disable-feature /featurename:TFTP
+dism /online /disable-feature /featurename:TelnetClient
+dism /online /disable-feature /featurename:TelnetServer
+
 
 sc stop "TlntSvr"
 sc config "TlntSvr" start= disabled
@@ -96,16 +200,136 @@ sc stop "RemoteRegistry"
     sc config "RemoteRegistry" start= disabled
 
 
+REM Reg Add
+
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /t
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d /1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f 
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeText /t REG_SZ /d "Lol noobz pl0x don't hax, thx bae"
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeCaption /t REG_SZ /d "Dnt hax me"
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
+
 REM Remote Disable
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f 
 netsh advfirewall firewall set rule group="remote desktop" new enable=No 
 
 
-REM Windows Scan
+REM Local Acct Pswd
 
-cd C:\ProgramData\Microsoft\Windows Defender\Platform\4*
-mpcmdrun -scan -scantype 2
+net accounts /minpwlen:10
+net accounts /maxpwage:30
+net accounts /minpwage:5
+
+
+REM SFC Scan
+
+Sfc.exe /scannow
+
+
+REM Windows Updates
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AutoInstallMinorUpdates /t REG_DWORD /d 1 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 4 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 4 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v DisableWindowsUpdateAccess /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ElevateNonAdmins /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoWindowsUpdate /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\Internet Communication Management\Internet Communication" /v DisableWindowsUpdateAccess /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\WindowsUpdate /v DisableWindowsUpdateAccess /t REG_DWORD /d 0 /f
+
+
+REM Reg Add Pt-2
+
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\LSASS.exe" /v AuditLevel /t REG_DWORD /d 00000008 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v RunAsPPL /t REG_DWORD /d 00000001 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v auditbaseobjects /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\PhishingFilter" /v EnabledV8 /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\PhishingFilter" /v EnabledV9 /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+reg ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
+reg ADD "HKU\.DEFAULT\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
+reg ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
 
 
 REM User Txt Doc Creation

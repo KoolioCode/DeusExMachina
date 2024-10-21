86bce3141ae5231bbdf06354d5ad8d7ce16931af
ProgWithLog <120297920+KoolioCode@users.noreply.github.com>
2024-10-21 14:12:30 -0500

    Split

--- DeusExMachina.bat
+++ DeusExMachina.bat
@@ -102,49 +102,17 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnec
 netsh advfirewall firewall set rule group="remote desktop" new enable=No 
 
 
-REM Fix Those Policies
+REM Windows Scan
 
-Function Parse-SecPol($CfgFile){ 
-    secedit /export /cfg "$CfgFile" | out-null
-    $obj = New-Object psobject
-    $index = 0
-    $contents = Get-Content $CfgFile -raw
-    [regex]::Matches($contents,"(?<=\[)(.*)(?=\])") | %{
-        $title = $_
-        [regex]::Matches($contents,"(?<=\]).*?((?=\[)|(\Z))", [System.Text.RegularExpressions.RegexOptions]::Singleline)[$index] | %{
-            $section = new-object psobject
-            $_.value -split "\r\n" | ?{$_.length -gt 0} | %{
-                $value = [regex]::Match($_,"(?<=\=).*").value
-                $name = [regex]::Match($_,".*(?=\=)").value
-                $section | add-member -MemberType NoteProperty -Name $name.tostring().trim() -Value $value.tostring().trim() -ErrorAction SilentlyContinue | out-null
-            }
-            $obj | Add-Member -MemberType NoteProperty -Name $title -Value $section
-        }
-        $index += 1
-    }
-    return $obj
-}
-
-Function Set-SecPol($Object, $CfgFile){
-   $SecPool.psobject.Properties.GetEnumerator() | %{
-        "[$($_.Name)]"
-        $_.Value | %{
-            $_.psobject.Properties.GetEnumerator() | %{
-                "$($_.Name)=$($_.Value)"
-            }
-        }
-    } | out-file $CfgFile -ErrorAction Stop
-    secedit /configure /db c:\windows\security\local.sdb /cfg "$CfgFile" /areas SECURITYPOLICY
-}
+cd C:\ProgramData\Microsoft\Windows Defender\Platform\4*
+mpcmdrun -scan -scantype 2
 
 
-$SecPool = Parse-SecPol -CfgFile C:\test\Test.cgf
-$SecPool.'System Access'.EnforcePasswordHistory = 3
-$SecPool.'System Access'.PasswordComplexity = 1
-$SecPool.'System Access'.MinimumPasswordLength = 8
-$SecPool.'System Access'.MaximumPasswordAge = 60
-$SecPool.'System Access'.MinimumPasswordAge = 60
+REM User Txt Doc Creation
 
-Set-SecPol -Object $SecPool -CfgFile C:\Test\Test.cfg
+cd Documents 
+    copy NUL Users.txt
+    notepad Users.txt
+cd..
 
 pause
---
+++ Policies.bat
@@ -0,0 +1,44 @@
+REM Fix Those Policies
+
+Function Parse-SecPol($CfgFile){ 
+    secedit /export /cfg "$CfgFile" | out-null
+    $obj = New-Object psobject
+    $index = 0
+    $contents = Get-Content $CfgFile -raw
+    [regex]::Matches($contents,"(?<=\[)(.*)(?=\])") | %{
+        $title = $_
+        [regex]::Matches($contents,"(?<=\]).*?((?=\[)|(\Z))", [System.Text.RegularExpressions.RegexOptions]::Singleline)[$index] | %{
+            $section = new-object psobject
+            $_.value -split "\r\n" | ?{$_.length -gt 0} | %{
+                $value = [regex]::Match($_,"(?<=\=).*").value
+                $name = [regex]::Match($_,".*(?=\=)").value
+                $section | add-member -MemberType NoteProperty -Name $name.tostring().trim() -Value $value.tostring().trim() -ErrorAction SilentlyContinue | out-null
+            }
+            $obj | Add-Member -MemberType NoteProperty -Name $title -Value $section
+        }
+        $index += 1
+    }
+    return $obj
+}
+
+Function Set-SecPol($Object, $CfgFile){
+   $SecPool.psobject.Properties.GetEnumerator() | %{
+        "[$($_.Name)]"
+        $_.Value | %{
+            $_.psobject.Properties.GetEnumerator() | %{
+                "$($_.Name)=$($_.Value)"
+            }
+        }
+    } | out-file $CfgFile -ErrorAction Stop
+    secedit /configure /db c:\windows\security\local.sdb /cfg "$CfgFile" /areas SECURITYPOLICY
+}
+
+
+$SecPool = Parse-SecPol -CfgFile C:\test\Test.cgf
+$SecPool.'System Access'.EnforcePasswordHistory = 3
+$SecPool.'System Access'.PasswordComplexity = 1
+$SecPool.'System Access'.MinimumPasswordLength = 8
+$SecPool.'System Access'.MaximumPasswordAge = 60
+$SecPool.'System Access'.MinimumPasswordAge = 60
+
+Set-SecPol -Object $SecPool -CfgFile C:\Test\Test.cfg
